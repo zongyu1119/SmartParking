c5464c99a9a39fb1fc095b75ddbbbcbe5dcbada8
zongyu <wuguoqiang0927@hotmail.com>
2022-03-13 22:28:04 +0800

    整个项目首次提交

---
+++ backend/SpringBoot/src/test/java/com/SmartParking/service/ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartParking.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
