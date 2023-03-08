f5984b15837681a0c8c63324240da84e8b2a2dcd
吴国强 <wuguoqiang0927@hotmail.com>
2023-03-08 20:30:14 +0800

    增加了ZyGlobalExceptionFilter

---
+++ backend/Zy.Webcore/zy.webcore.Share.Application/Filter/ZyGlobalExceptionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using zy.webcore.Share.Application.Utilitys;
+
+namespace zy.webcore.Share.Application.Filter
+{
+    public class ZyGlobalExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger<ZyGlobalExceptionFilter> _logger;
+        public ZyGlobalExceptionFilter()
+        {
+            _logger = ServiceLocator.Instance.GetService<ILogger<ZyGlobalExceptionFilter>>();
+        }
+        public void OnException(ExceptionContext context)
+        {
+            context.Result=new Result
+            _logger.LogError("[Error]" + context.Exception.Message ?? "");
+        }
+    }
+}
